6af76fb3b75e36a6fdd6be389293330e5fb63111
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-04-29 22:46:38 +0700

    Template engine (handlebars)

---
+++ block/src/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars').engine;
+const app = express();
+const port = 3000;
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+console.log('PATH:', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  // var a = 1;
+  // var b = 2;
+  // var c = a+b;
+  // return res.send(`<h1>'Hello World!'</h1>`)
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+ 
+  res.render('news');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
+
+
+
