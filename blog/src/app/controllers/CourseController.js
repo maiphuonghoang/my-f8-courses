70eeadebdc5f6f1d2238868ef9ff5a4fc280acfa
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-05-04 14:08:19 +0700

    when soft delete add attribute deleted:true, override method show document deleted:false, add atrribute deletedAt

--- blog/src/app/controllers/CourseController.js
+++ blog/src/app/controllers/CourseController.js
@@ -21,42 +21,49 @@ class CourseController {
 
   // [POST] /courses/store
   store(req, res, next) {
-    // console.log(res.json(req.body)); 
-    const formData = req.body
-    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`
-    const course = new Course(formData);//đưa đối tượng {} muốn lưu vào 
-    course.save()
-      .then(() => res.redirect('/')) 
-      .catch(err => {
-
-      })    
+    // console.log(res.json(req.body));
+    const formData = req.body;
+    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    const course = new Course(formData); //đưa đối tượng {} muốn lưu vào
+    course
+      .save()
+      .then(() => res.redirect("/"))
+      .catch((err) => {});
   }
 
-    // [GET] /courses/:id/edit
-    edit(req, res, next) {
-      Course.findById(req.params.id)
-      .then(course => res.render("courses/edit", {
-        // cầm course ném sang view ở đối số thứ 2 
-        course: mongooseToObject(course)
-      }))
-      .catch(next)
-      
-    }
+  // [GET] /courses/:id/edit
+  edit(req, res, next) {
+    Course.findById(req.params.id)
+      .then((course) =>
+        res.render("courses/edit", {
+          // cầm course ném sang view ở đối số thứ 2
+          course: mongooseToObject(course),
+        })
+      )
+      .catch(next);
+  }
 
-    // [PUT] /courses/:id
-    update(req, res, next) {
-      // res.json(req.body)
-      // mongoose Models Updating
-      Course.updateOne({_id: req.params.id}, req.body)
-        .then(()=> res.redirect('/me/stored-courses'))
-        .catch(next)
-    }
-    // [DELETE] /courses/:id
-    destroy(req, res, next) {
-      Course.deleteOne({_id: req.params.id})
-        .then(()=> res.redirect('back'))
-        .catch(next)
-    }
-
+  // [PUT] /courses/:id
+  update(req, res, next) {
+    // res.json(req.body)
+    // mongoose Models Updating
+    Course.updateOne({ _id: req.params.id }, req.body)
+      .then(() => res.redirect("/me/stored-courses"))
+      .catch(next);
+  }
+  // [DELETE] /courses/:id => HARD DELETE
+  /*
+  destroy(req, res, next) {
+    Course.deleteOne({ _id: req.params.id })
+      .then(() => res.redirect("back"))
+      .catch(next);
+  }
+  */
+  // [DELETE] /courses/:id => SOFT DELETE
+  destroy(req, res, next) {
+    Course.delete({ _id: req.params.id })// add/set attribute delete:true 
+      .then(() => res.redirect("back"))
+      .catch(next);
+  }
 }
 module.exports = new CourseController();
--- blog/src/app/models/Course.js
+++ blog/src/app/models/Course.js
@@ -1,26 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 //add plugin mongoose-slug-generator
-const slug = require('mongoose-slug-updater')
-const mongooseDelete = require('mongoose-delete');
+const slug = require("mongoose-slug-updater");
+const mongooseDelete = require("mongoose-delete");
 
-const Course = new Schema({
-  name: {type: String, require: true},
-  description: {type: String},
-  image: {type: String},
-  videoId: {type: String},
-  level: {type: String},
-  // createdAt: { type: Date, default: Date.now },
-  // updatedAt: { type: Date, default: Date.now },
-  slug: {type: String, slug: 'name', unique: true}
+const Course = new Schema(
+  {
+    name: { type: String, require: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String },
+    level: { type: String },
+    // createdAt: { type: Date, default: Date.now },
+    // updatedAt: { type: Date, default: Date.now },
+    slug: { type: String, slug: "name", unique: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-}, {
-  timestamps: true
-});
-
-//  Add plugin 
+//  Add plugin
 mongoose.plugin(slug);
-Course.plugin(mongooseDelete)
-
-module.exports = mongoose.model('Course', Course)
+Course.plugin(mongooseDelete, {
+  overrideMethods: "all", // show only the document that deleted attribute = false,
+  deletedAt: true, // add attribute time when delete
+}),
+  (module.exports = mongoose.model("Course", Course));
