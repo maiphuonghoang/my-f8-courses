d2ad43f7478999b4cb38b63572a04d07011ae9f6
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-05-05 17:17:08 +0700

    delete multiple checkbox with

--- blog/src/app/controllers/CourseController.js
+++ blog/src/app/controllers/CourseController.js
@@ -78,5 +78,22 @@ class CourseController {
       .then(() => res.redirect("back"))
       .catch(next);
   }
+
+  // [POST] /courses/handle-form-actions
+  handleFormActions(req, res, next) {
+    // res.json(req.body);
+    switch(req.body.action){
+      case 'delete':         
+        //xóa mềm 1 mảng các course 
+        Course.delete({ _id: {$in: req.body.courseIds} }) 
+          .then(() => res.redirect("back"))
+          .catch(next);
+        break;
+      default:
+        res.json({message: 'Action invalid'})
+    }
+  }
+
+
 }
 module.exports = new CourseController();
